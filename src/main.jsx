56b3b05b8c71369dffb5404cbb37173d3e08ffc2
Andre Ignorosa <andreiglu@hotmail.com>
2023-12-05 21:35:48 -0600

    Hash Router agregado

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HeroesApp } from './HeroesApp.jsx'
+import { HashRouter } from 'react-router-dom'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <HashRouter>
       {/* Se agrega el componente padre que tendrá toda la aplicación */}
-      <HeroesApp/>    
+      <HeroesApp/>  
+    </HashRouter>
+        
   </React.StrictMode>,
 )
